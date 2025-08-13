21696ec4fbc7a737b51a61350103dc6e71705a0f
DuongNV <duong.nv@kynaforkids.vn>
2025-08-14 00:03:52 +0700

    complete user module

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -2,18 +2,23 @@ import {BaseService} from '@/core/base/base.service';
 import {UserRepository} from './user.repository';
 import {AppError, ValidationError} from "@/core/middleware/errorHandler";
 import bcrypt from "bcrypt";
+import {UserDTO} from "@/modules/user/user.dto";
 
 export class UserService extends BaseService<UserRepository> {
     constructor(repository: UserRepository) {
         super(repository);
     }
 
-    async create(data: any) {
-        data.password = await bcrypt.hash(data.password, 10);
-        const existing = await this.repository.findByUsername(data.username);
-        if (existing) throw new ValidationError([
-            {path: ['name'], message: 'Username already exists'},
-        ]);
+    async create(data: UserDTO) {
+        if (data.password) {
+            data.password = await bcrypt.hash(data.password, 10);
+        }
+        if (data.username) {
+            const existing = await this.repository.findByUsername(data.username);
+            if (existing) throw new ValidationError([
+                {path: ['name'], message: 'Username already exists'},
+            ]);
+        }
         const createdUser = await this.repository.create(data);
 
         if (!createdUser?.id) throw new AppError("Create user failed!");
@@ -24,11 +29,13 @@ export class UserService extends BaseService<UserRepository> {
         }
     }
 
-    async update(id: number, data: any) {
-        const existing = await this.repository.findByUsername(data.username);
-        if (existing && existing.id !== id) throw new ValidationError([
-            {path: ['name'], message: 'Username already exists'},
-        ]);
+    async update(id: number, data: UserDTO) {
+        if (data.username) {
+            const existing = await this.repository.findByUsername(data.username);
+            if (existing && existing.id !== id) throw new ValidationError([
+                {path: ['name'], message: 'Username already exists'},
+            ]);
+        }
         if (data.password) {
             data.password = await bcrypt.hash(data.password, 10);
         }
--- backend/src/modules/user/user.validator.ts
+++ backend/src/modules/user/user.validator.ts
@@ -1,20 +1,29 @@
 import { z } from 'zod';
 import {BaseValidator} from "@/core/base/base.validator";
 
-const createAndUpdateValidator = z.object({
-    username: z
-        .string("Name must be a string")
-        .nonempty("Name is required")
-        .max(255, { message: 'name must be at most 255 characters' })
-    ,
-    password: z
-        .string("Password must be a string")
-        .nonempty("Password is required")
-        .max(255, { message: 'name must be at most 255 characters' })
-});
 export class UserValidator extends BaseValidator {
-    onCreate = createAndUpdateValidator
-    onUpdate = createAndUpdateValidator
+    onCreate = z.object({
+        username: z
+            .string("Name must be a string")
+            .nonempty("Name is required")
+            .max(255, { message: 'name must be at most 255 characters' })
+        ,
+        password: z
+            .string("Password must be a string")
+            .nonempty("Password is required")
+            .max(255, { message: 'name must be at most 255 characters' })
+    });
+    onUpdate = z.object({
+        username: z
+            .string("Name must be a string")
+            .max(255, { message: 'name must be at most 255 characters' })
+            .optional()
+        ,
+        password: z
+            .string("Password must be a string")
+            .max(255, { message: 'name must be at most 255 characters' })
+            .optional()
+    });
 
     static onLogin = z.object({
         username: z
